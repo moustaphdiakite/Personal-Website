139afc1c4c7ad23b5b82e0fb6c4049bcc3105aca
Moustaph Diakite <63529813+moustaphdiakite@users.noreply.github.com>
2023-03-25 16:53:06 -0300

    Add event listener to document to close the nav bar when clicked outsideAdd

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 var menuIcon = document.querySelector('.menu-icon');
 var navBar = document.querySelector('.navbar');
+var header = document.querySelector('header');
 menuIcon.addEventListener('click', function (_event) {
     navBar.classList.toggle('inactive');
+});
+document.addEventListener('click', function (event) {
+    var isInside = header.contains(event.target);
+    if (!isInside) {
+        navBar.classList.add('inactive');
+        3;
+    }
 });
--- js/index.ts
+++ js/index.ts
@@ -1,5 +1,14 @@
 const menuIcon = document.querySelector('.menu-icon') as HTMLElement;
 const navBar = document.querySelector('.navbar') as HTMLElement;
+const header = document.querySelector('header') as HTMLElement;
 menuIcon.addEventListener('click', (_event) => {
     navBar.classList.toggle('inactive');
+});
+
+document.addEventListener('click', (event) => {
+    const isInside = header.contains(event.target as Node);
+    if (!isInside) {
+        navBar.classList.add('inactive');
+        3;
+    }
 });
